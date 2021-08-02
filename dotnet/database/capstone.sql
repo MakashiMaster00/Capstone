01f6e2a389796fad669a1d2e2d01871541986a1c
JakeGiuliano <jacogiul@gmail.com>
2021-08-02 11:56:27 -0400

    Added Properties table to SQL

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -20,12 +20,33 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	salt varchar(200) NOT NULL,
-	user_role varchar(50) NOT NULL
+	user_role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
 
---populate default data
+CREATE TABLE properties (
+	property_id int IDENTITY(1,1) NOT NULL,
+	landlord_id int NOT NULL,
+	description varchar(500) NOT NULL,
+	address_line_one varchar(50) NOT NULL,
+	address_line_two varchar(50) NULL,
+	state_abbreviation varchar(2) NOT NULL,
+	city varchar(50) NOT NULL,
+	zip_code int NOT NULL,
+	price money NOT NUll,
+	date_available datetime NOT NULL,
+	available bit NOT NULL,
+	beds int NOT NULL,
+	baths decimal NOT NULL,
+	CONSTRAINT PK_property_id PRIMARY KEY (property_id),
+	CONSTRAINT FK_landlord_id FOREIGN KEY (landlord_id) REFERENCES users (user_id)
+)
+--TODO Photo table links to property id
+
+--populate default data for users
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
+--populate default data for properties
 
 GO
