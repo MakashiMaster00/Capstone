3d91df0df32c805103ac94e048ed1bddbc81c911
Patrick Alcox <patalcox123@gmail.com>
2021-08-03 17:40:44 -0400

    add property form linked to data property model

--- vue/src/views/AddProperty.vue
+++ vue/src/views/AddProperty.vue
@@ -2,50 +2,70 @@
 
 <template>
   <div>
-      <section>
-        <b-field label="Name">
-            <b-input v-model="name"></b-input>
-        </b-field>
+    <section>
+      <b-field label="Street Address">
+        <b-input v-model="property.addressLineOne"></b-input>
+      </b-field>
 
-        <b-field label="Email"
-            type="is-danger"
-            message="This email is invalid">
-            <b-input type="email"
-                value=""
-                maxlength="30">
-            </b-input>
-        </b-field>
-
-        <b-field label="Username"
-            type="is-success"
-            message="This username is available">
-            <b-input value="johnsilver" maxlength="30"></b-input>
-        </b-field>
-
-        <b-field label="Password">
-            <b-input type="password"
-                value="iwantmytreasure"
-                password-reveal>
-            </b-input>
-        </b-field>
-
-        <b-field label="Message">
-            <b-input maxlength="200" type="textarea"></b-input>
-        </b-field>
+      <b-field label="Address Line 2 (Optional)">
+        <b-input v-model="property.addressLineTwo"> </b-input>
+      </b-field>
+      <b-field label="City">
+        <b-input v-model="property.city"></b-input>
+      </b-field>
+      <b-field label="State">
+        <b-input v-model="property.state"></b-input>
+      </b-field>
+      <b-field label="Zip Code">
+        <b-input maxlength = "5" v-model="property.zipCode"></b-input>
+      </b-field>
+      <b-field label="Beds">
+        <b-input v-model="property.beds"></b-input>
+      </b-field>
+ 
+      <b-field label="Baths">
+        <b-input v-model="property.baths"> </b-input>
+      </b-field>
+      <b-field label="Price">
+        <b-input v-model="property.price"></b-input>
+      </b-field>
+      <b-field label="Date Available">
+        <b-input type="date" v-model="property.dateAvailable"></b-input>
+      </b-field>
+        <b-field label="Description">
+        <b-input maxlength ="200" type="textarea" v-model="property.description"></b-input>
+      </b-field>
+      <div class="buttons">
+            <b-button style="background-color:powderblue" type="submit" expanded>Submit</b-button>
+        </div>
     </section>
   </div>
 </template>
 
 <script>
 export default {
-     data() {
-            return {
-                name: 'John Silver'
-            }
-        }
-}
+  data() {
+    return {
+      property: {
+        propertyId: 0,
+        landlordId: 0,
+        description: "",
+        addressLineOne: "",
+        addressLineTwo: "",
+        city: "",
+        state: "",
+        zipCode: "",
+        price: 0,
+        dateAvailable: "",
+        available: true,
+        beds: 0,
+        baths: 0,
+      },
+    };
+  },
+  
+};
 </script>
 
 <style>
-
 </style>
