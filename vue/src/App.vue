35c4408f06d66f6f66c6a02e1c7ab2775965c6a1
JakeGiuliano <jacogiul@gmail.com>
2021-08-05 13:34:07 -0400

    Started the home page layout and style

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,49 +2,66 @@
 
 <template>
   <div id="app">
-    <div id="nav">
-      <div id="app" class="container">
-        <b-navbar>
-          <template #brand>
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-              <img
-                src="https://thumbs.dreamstime.com/b/apartment-building-logo-design-inspiration-vector-template-apartment-logo-icon-design-vector-template-158106696.jpg"
-                alt="Lightweight UI components for Vue.js based on Bulma"
-              />
-            </b-navbar-item>
-          </template>
-          <template #start>
-            <b-navbar-item tag="router-link" :to="{ name: 'addproperty' }">
-              Add A Property
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ name: 'properties' }">
-              View Listings
-            </b-navbar-item>
-            <b-navbar-dropdown label="Info">
-              <b-navbar-item href="#"> About </b-navbar-item>
-              <b-navbar-item href="#"> Contact </b-navbar-item>
-            </b-navbar-dropdown>
-          </template>
-
-          <template #end>
-            <b-navbar-item tag="div">
-              <div class="buttons">
-                <a class="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a class="button is-light"> Log in </a>
-              </div>
-            </b-navbar-item>
-          </template>
-        </b-navbar>
-      </div>
-
-      <router-link
-        v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token != ''"
-        >Logout</router-link
-      >
+    <div id="navbar">
+      <b-navbar>
+        <template #brand>
+          <img
+            id="logo"
+            src="../src/assets/images/logosingular.jpg"
+            alt="Plant With Logo"
+          />
+        </template>
+        <template #start>
+          <b-navbar-item id="homepage" tag="router-link" :to="{ path: '/' }">
+            Home
+          </b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ name: 'addproperty' }">
+            Add A Property
+          </b-navbar-item>
+          <b-navbar-item tag="router-link" :to="{ name: 'properties' }">
+            View Listings
+          </b-navbar-item>
+          <b-navbar-dropdown label="Info">
+            <b-navbar-item href="#"> About </b-navbar-item>
+            <b-navbar-item href="#"> Contact </b-navbar-item>
+          </b-navbar-dropdown>
+          <b-navbar-item tag="div">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light"> Log in </a>
+            </div>
+          </b-navbar-item>
+        </template>
+      </b-navbar>
     </div>
+
+    <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"
+      >Logout</router-link
+    >
     <router-view />
   </div>
 </template>
+
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+#navbar {
+  position: fixed;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+#registration {
+  justify-content: right;
+}
+#logo {
+  width: 125px;
+}
+</style>
--- vue/src/Coding/Websites/CodePen_Export_wvgLxom/README.markdown
+++
@@ -1,5 +0,0 @@
-# 
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/JakeGiuliano/pen/wvgLxom](https://codepen.io/JakeGiuliano/pen/wvgLxom).
-
-
