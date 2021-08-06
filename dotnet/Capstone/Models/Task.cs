6bffa4ab739ca9553d66b26fc4ee2c4c04265171
reggie callender <regjcallender@gmail.com>
2021-08-06 13:45:02 -0400

    model changes

--- dotnet/Capstone/Models/Task.cs
+++ dotnet/Capstone/Models/Task.cs
@@ -12,6 +12,12 @@ namespace Capstone.Models
         public int TaskId { get; set; }
         public int PropertyId { get; set; }
         public bool IsUrgent { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime DateEntered { get; set; }
+        public DateTime DateScheduled { get; set; }
+        public string TaskStatus { get; set; }
+        //landlord id is not in mssql task table using a join to properties
         public int LandlordId { get; set; }
+
     }
 }
