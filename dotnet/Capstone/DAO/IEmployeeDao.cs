619087393e699dad2a5ea5f4802fe07facac1cc1
Patrick Alcox <patalcox123@gmail.com>
2021-08-10 11:17:16 -0400

    updating move to completed

--- dotnet/Capstone/DAO/IEmployeeDao.cs
+++ dotnet/Capstone/DAO/IEmployeeDao.cs
@@ -8,7 +8,7 @@ namespace Capstone.DAO
     public interface IEmployeeDao
     {
         List<Employee> GetEmployeesByLandlordId(int id);
-<<<<<<< HEAD
+
 
 
         int AddEmployee(Employee employee);
@@ -16,9 +16,7 @@ namespace Capstone.DAO
 
 
 
-=======
 
->>>>>>> 5b4f26e15d42f23b9a784ba3206788c761333509
         Employee GetEmployee(int id);
 
     }
