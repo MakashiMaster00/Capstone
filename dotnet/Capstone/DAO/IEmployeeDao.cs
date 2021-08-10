5369983cb1d715a5b765676d97ddc8a1730053ba
reggie callender <regjcallender@gmail.com>
2021-08-10 15:03:04 -0400

    employee add/delete

--- dotnet/Capstone/DAO/IEmployeeDao.cs
+++ dotnet/Capstone/DAO/IEmployeeDao.cs
@@ -8,18 +8,8 @@ namespace Capstone.DAO
     public interface IEmployeeDao
     {
         List<Employee> GetEmployeesByLandlordId(int id);
-<<<<<<< HEAD
-
-
         int AddEmployee(Employee employee);
         int DeleteEmployee(int id);
-
         Employee GetEmployee(int id);
-
-=======
-        Employee GetEmployee(int id);
-        int AddEmployee(Employee employee);
-        int DeleteEmployee(int id);
->>>>>>> 678510713a4efc2e7d4aa747022741f88e5b3c38
     }
 }
